37b4292346be17ca1fb539fb495746dede1d0f7a
W4H33D <85181215+W4H33D@users.noreply.github.com>
2021-09-10 19:40:34 +0500

    Create Data_Types.py

---
+++ Data_Types.py
@@ -0,0 +1,17 @@
+# Data Types
+
+Here we made a program that print four data types of python
+
+```
+#!/usr/bin/python3
+
+a = 10  # Data Type = int
+
+b = 10.1 # Data Type = float
+
+c = 'Hello' # Data Type = string
+
+d = "Hello World" # Data Type = This is also a String Data Type
+
+print(" Data Type = int:",a,"\n Data Type = float:",b,"\n Data Type = string:",c,"\n Data Type = String:",d)
+```
