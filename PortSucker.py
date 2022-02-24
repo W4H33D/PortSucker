0850053353cf030cb72d44cec764fccd13e07bf6
W4H33D <85181215+W4H33D@users.noreply.github.com>
2022-02-24 23:27:40 +0500

    Add banner in source code

--- PortSucker.py
+++ PortSucker.py
@@ -25,9 +25,22 @@ if args.port[0] <= 0 or args.port[1] > 65535:
 
 
 # <--------------------------------------------- PortSucker Banner ---------------------------------------------->
-f = open("banner.txt", "r")
-print(f.read())
-f.close()
+print(
+    '''  
+    @@@@@@@    @@@@@@   @@@@@@@   @@@@@@@   @@@@@@   @@@  @@@   @@@@@@@  @@@  @@@  @@@@@@@@  @@@@@@@   
+    @@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@  @@@@@@@   @@@  @@@  @@@@@@@@  @@@  @@@  @@@@@@@@  @@@@@@@@  
+    @@!  @@@  @@!  @@@  @@!  @@@    @@!    !@@       @@!  @@@  !@@       @@!  !@@  @@!       @@!  @@@  
+    !@!  @!@  !@!  @!@  !@!  @!@    !@!    !@!       !@!  @!@  !@!       !@!  @!!  !@!       !@!  @!@  
+    @!@@!@!   @!@  !@!  @!@!!@!     @!!    !!@@!!    @!@  !@!  !@!       @!@@!@!   @!!!:!    @!@!!@!   
+    !!@!!!    !@!  !!!  !!@!@!      !!!     !!@!!!   !@!  !!!  !!!       !!@!!!    !!!!!:    !!@!@!    
+    !!:       !!:  !!!  !!: :!!     !!:         !:!  !!:  !!!  :!!       !!: :!!   !!:       !!: :!!   
+    :!:       :!:  !:!  :!:  !:!    :!:        !:!   :!:  !:!  :!:       :!:  !:!  :!:       :!:  !:!  
+     ::       ::::: ::  ::   :::     ::    :::: ::   ::::: ::   ::: :::   ::  :::   :: ::::  ::   :::  
+     :         : :  :    :   : :     :     :: : :     : :  :    :: :: :   :   :::  : :: ::    :   : :  
+                                                                                                       
+                                                                                          (By W4H33D)
+    '''
+    )
 # ----------------------------------------------------------------------------------------------------------------
 
 st_time = time.time()
@@ -44,6 +57,60 @@ def scanner(address, scan_port):
         s = socket.socket()
         s.settimeout(0.5)
         s.connect((address, scan_port))
+        try:
+            banner = get_banner(s)
+            print(str(scan_port) + '\t' + 'Open\t' + str(banner.decode().strip('\n')))
+        except:
+            print(str(scan_port) + '\tOpen')
+    except:
+        pass
+# <---------------------------------------------->
+
+
+# <--------checking IP and also resolve the Ip address------------------->
+def resolve_ip(ip):
+    try:
+        IP(ip)
+        return ip
+    except ValueError:
+        try:
+            return socket.gethostbyname(ip)
+        except:
+            print("[!!] Name Resolution Failed. Check The Target ("+ip+")")
+            sys.exit()
+# ------------------------------------------------------------------------
+
+
+# <-------------Multiple Target Scanner-----------------------------------
+def multi_scanner(targets):
+    multi_ip = resolve_ip(targets)
+    print("\n[Scanning Target] " + str(targets) + "(" + str(multi_ip) + ")")
+    print('Ports\t' + 'Status\t' + 'service')
+    for multi_port in range(range_port_1, range_port_2 + 1):
+        scanner(multi_ip, multi_port)
+# ----------------------------------------------------------------------->
+
+
+# <--------------------------- Main Working ------------------------------->
+ipaddress = args.target
+range_port_1 = int(args.port[0])
+range_port_2 = int(args.port[1])
+
+if ',' in ipaddress:
+    for ip_add in ipaddress.split(','):
+        multi_scanner(ip_add)
+else:
+    resolved_ip = resolve_ip(ipaddress)
+    print("[Scanning Target] " + str(ipaddress) + "(" + str(resolved_ip) + ")\n")
+    print('Ports\t' + 'Status\t' + 'service')
+    for port in range(range_port_1, range_port_2 + 1):
+        scanner(resolved_ip, port)
+# ----------------------------------------------------------------------------
+        
+ed_time = time.time()
+t_time = ed_time - st_time
+
+print("Scan Complete in " + str(round(t_time)) + " sec")
         try:
             banner = get_banner(s)
             print(str(scan_port) + '\t' + 'Open\t' + str(banner.decode().strip('\n')))
