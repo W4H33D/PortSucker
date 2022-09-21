19824c1f35bd5c3a97c8cb4cb27ee02507ce486e
W4H33D <85181215+W4H33D@users.noreply.github.com>
2022-09-21 20:08:10 +0500

    Fix Syntax Errors and Remove Duplicate Functions

--- PortSucker.py
+++ PortSucker.py
@@ -91,52 +91,6 @@ def multi_scanner(targets):
 # ----------------------------------------------------------------------->
 
 
-# <--------------------------- Main Working ------------------------------->
-ipaddress = args.target
-range_port_1 = int(args.port[0])
-range_port_2 = int(args.port[1])
-
-if ',' in ipaddress:
-    for ip_add in ipaddress.split(','):
-        multi_scanner(ip_add)
-else:
-    resolved_ip = resolve_ip(ipaddress)
-    print("[Scanning Target] " + str(ipaddress) + "(" + str(resolved_ip) + ")\n")
-    print('Ports\t' + 'Status\t' + 'service')
-    for port in range(range_port_1, range_port_2 + 1):
-        scanner(resolved_ip, port)
-# ----------------------------------------------------------------------------
-        
-ed_time = time.time()
-t_time = ed_time - st_time
-
-print("Scan Complete in " + str(round(t_time)) + " sec")
-# <---------------------------------------------->
-
-
-# <--------checking IP and also resolve the Ip address------------------->
-def resolve_ip(ip):
-    try:
-        IP(ip)
-        return ip
-    except ValueError:
-        try:
-            return socket.gethostbyname(ip)
-        except:
-            print("[!!] Name Resolution Failed. Check The Target ("+ip+")")
-            sys.exit()
-# ------------------------------------------------------------------------
-
-
-# <-------------Multiple Target Scanner-----------------------------------
-def multi_scanner(targets):
-    multi_ip = resolve_ip(targets)
-    print("\n[Scanning Target] " + str(targets) + "(" + str(multi_ip) + ")")
-    print('Ports\t' + 'Status\t' + 'service')
-    for multi_port in range(range_port_1, range_port_2 + 1):
-        scanner(multi_ip, multi_port)
-# ----------------------------------------------------------------------->
-
 
 # <--------------------------- Main Working ------------------------------->
 ipaddress = args.target
